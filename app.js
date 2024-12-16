47182af4c650330801702d658a4d7fbf97fe8034
zoheballadin <zoheballadin1@gmail.com>
2024-12-16 14:55:09 +0530

    zz

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('welcome to hello world route');
+  res.send('hello world');
 });
 
 app.listen(port, () => console.log(`server running on ${port}`));
