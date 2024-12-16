e95faf1200e32a539c24efb90f726467acd44bd3
zoheballadin <zoheballadin1@gmail.com>
2024-12-17 00:39:49 +0530

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('welcome to hello world router');
+  res.send('welcome to CI/CD');
 });
 
 app.listen(port, () => console.log(`server running on ${port}`));
