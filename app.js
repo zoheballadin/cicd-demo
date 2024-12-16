95e51efe228e9753b125be79a904f4cc09f2c400
zoheballadin <zoheballadin1@gmail.com>
2024-12-16 08:44:17 +0530

    zz

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('welcome to home');
