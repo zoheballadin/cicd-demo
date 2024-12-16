518370001282a289df61eae158e8f63009c2dbd1
zoheballadin <zoheballadin1@gmail.com>
2024-12-16 08:41:04 +0530

    zz

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('welcome to home');
+});
+
+app.listen(port, () => console.log(`server running on ${port}`));
